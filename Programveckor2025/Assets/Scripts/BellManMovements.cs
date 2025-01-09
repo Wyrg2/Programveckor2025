623172caeb24df350d80cc93a5431628b241c06e
Wyrg2 <felix.rosenkron@elev.ga.lbs.se>
2025-01-09 11:27:44 +0100

    Move
    No longer Stephen Hawking

--- Programveckor2025/Assets/Scripts/BellManMovements.cs
+++ Programveckor2025/Assets/Scripts/BellManMovements.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class BellManMovements : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D rb;
     void Start()
     {
-        
+        transform.position = new Vector2(0, -2.8f);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.LeftArrow)))
+        {
+            rb.velocity += new Vector2(-50, 0);
+        }
+        if (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.RightArrow)))
+        {
+            rb.velocity += new Vector2(50, 0);
+        }
     }
 }
