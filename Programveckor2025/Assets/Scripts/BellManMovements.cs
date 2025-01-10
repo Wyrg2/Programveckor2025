6b13e67f6a747d4d4463510c23d8e628ad6d6965
ReubenKud <kudrenreuben@gmail.com>
2025-01-10 11:13:57 +0100

    Jumping and shit
    ni

--- Programveckor2025/Assets/Scripts/BellManMovements.cs
+++ Programveckor2025/Assets/Scripts/BellManMovements.cs
@@ -6,39 +6,112 @@ public class BellManMovements : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D rb;
+    
     public float BellManSpeed;
+    public float BellmanMaxSpeed;
+    public float Retardera;
+    public float jumpHeight;
+    public float fallMultiplier;
+    
+    bool isGrounded;
+    bool canDoubleJump;
+
+    Vector2 gravity;
+
     void Start()
     {
-        transform.position = new Vector2(0, -2.8f);
-        rb = GetComponent<Rigidbody2D>();
+         rb = GetComponent<Rigidbody2D>();
+         gravity = new Vector2(rb.velocity.x, jumpHeight/2);
     }
 
     // Update is called once per frame
     void Update()
     {
+        falling();
+        
+        //Movement
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) 
         {
-            if(BellManSpeed< 10)
+            falling();
+            
+            isGrounded = true;
+
+            if(BellManSpeed< BellmanMaxSpeed)
             {
-                BellManSpeed += 0.2f;
+                BellManSpeed += Retardera;
             }
             rb.velocity = new Vector2(-BellManSpeed, 0);
             if (BellManSpeed > 0)
             {
-                BellManSpeed -= 0.2f;
+                BellManSpeed -= Retardera;
             }
+            
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            if(BellManSpeed < 10) 
+
+            falling();
+            
+            isGrounded = true;
+            
+            if(BellManSpeed < BellmanMaxSpeed) 
             {
-                 BellManSpeed += 0.2f;
+                 BellManSpeed += Retardera;
             }
             rb.velocity = new Vector2(BellManSpeed,0);
             if(BellManSpeed > 0)
             {
-                BellManSpeed -= 0.2f;
+                BellManSpeed -= Retardera;
             }
+            
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded==true)
+        {
+            Jump();
+            falling();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDoubleJump == true)
+        {
+            DoubleJump();
+            canDoubleJump = false;
+            falling();
+        } 
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+
+    void DoubleJump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, (jumpHeight * 0.75f));
+    }
+
+    void falling()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity -= gravity * fallMultiplier * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            canDoubleJump = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+            canDoubleJump = true;
         }
     }
 }
