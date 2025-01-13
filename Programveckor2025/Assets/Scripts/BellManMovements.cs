954cad39ff303551deff5c591f99590096978430
Wyrg2 <felix.rosenkron@elev.ga.lbs.se>
2025-01-13 11:22:58 +0100

    Bell Man is not a cripple (For real this time)
    penis cock balls in my ass

--- Programveckor2025/Assets/Scripts/BellManMovements.cs
+++ Programveckor2025/Assets/Scripts/BellManMovements.cs
@@ -4,109 +4,49 @@ using UnityEngine;
 
 public class BellManMovements : MonoBehaviour
 {
-    // Start is called before the first frame update
     Rigidbody2D rb;
-    
-    
-    public float BellManSpeed;
-    public float BellmanMaxSpeed;
-    public float jumpHeight;
-    public float fallMultiplier;
+    private float Speed;
     private float Direction;
-    
-    bool isGrounded;
-    bool canDoubleJump;
-
-    Vector2 gravity;
-
+    int JumpsLeft;
+    // Start is called before the first frame update
     void Start()
     {
-         rb = GetComponent<Rigidbody2D>();
-         gravity = new Vector2(rb.velocity.x, jumpHeight/2);
-        Direction = 0;
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = new Vector2(0, 0);
+        Speed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Left and right movement script
         Direction = (Input.GetAxis("Horizontal"));
-       
-        falling();
-        
-        //Movement
-       if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) 
-       {
-         if(BellManSpeed < 10)
-         {
-             BellManSpeed += 0.5f;
-         }
-       }
-       else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-       {
-            if (BellManSpeed < 10)
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Direction = (Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Direction = (Input.GetAxis("Horizontal"));
+        }
+        rb.velocity = new Vector2(Direction * 10, rb.velocity.y);
+
+        //Jump Script
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.LeftArrow))
+        { 
+            if(JumpsLeft > 0)
             {
-                BellManSpeed += 0.5f;
+                rb.velocity = new Vector2(rb.velocity.x, 10);
+                JumpsLeft -= 1;
             }
         }
-        else
-        {
-            BellManSpeed -= 0.5f;
-        }
-
-
-        rb.velocity = new Vector2(BellManSpeed * Direction * Time.deltaTime, rb.velocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded==true)
-        {
-            Jump();
-            falling();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && canDoubleJump == true)
-        {
-            DoubleJump();
-            canDoubleJump = false;
-            falling();
-        } 
     }
-
-    void Jump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-    }
-
-    void DoubleJump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, (jumpHeight * 0.75f));
-    }
-
-    void falling()
-    {
-        if (rb.velocity.y < 0)
-        {
-            rb.velocity -= gravity * fallMultiplier * Time.deltaTime;
-        }
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        // detects if the player is touching the ground so the player will not be able to jump forever
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true;
-            canDoubleJump = false;
+            JumpsLeft = 2;
         }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            isGrounded = false;
-            canDoubleJump = true;
-        }
-       if (Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 25);
-        }
-
     }
 }
