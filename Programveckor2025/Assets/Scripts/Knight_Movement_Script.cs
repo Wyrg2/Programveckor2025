78c9fe09feb25440ca7cefa692a0d55b1794bf37
Wyrg2 <felix.rosenkron@elev.ga.lbs.se>
2025-01-15 14:01:27 +0100

    rgneiuhfoiqekdf

--- Programveckor2025/Assets/Scripts/Knight_Movement_Script.cs
+++ Programveckor2025/Assets/Scripts/Knight_Movement_Script.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Knight_Movement_Script : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public GameObject BellMan;
+    private int KnightDirection;
+    Animator animate;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = new Vector2(10.7f, -1.09f);
+        KnightDirection = -10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector2((transform.position.x) + (KnightDirection * Time.deltaTime), -1.09f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Border"))
+        {
+            KnightDirection *= -1;
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+        }
     }
 }
