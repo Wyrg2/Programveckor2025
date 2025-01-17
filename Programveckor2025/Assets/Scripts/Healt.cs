35c67db95135d5801a7d7befbc99c1ee02ee94f6
KingJohnnyJoestar <johannes.tarakjian@elev.ga.lbs.se>
2025-01-17 13:47:05 +0100

    s
    s

--- Programveckor2025/Assets/Scripts/Healt.cs
+++ Programveckor2025/Assets/Scripts/Healt.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Healt : MonoBehaviour
 {
@@ -16,9 +17,10 @@ public class Healt : MonoBehaviour
     public void TakeDamage(int amount)
     {
         health -= amount;
+        print("test");
         if(health <= 0)
         {
-            Destroy(gameObject);
+               // SceneManager.LoadScene(0);
         }
     }
 
